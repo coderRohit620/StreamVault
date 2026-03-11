f88e082bb4ca1dbae649a8113e93965fac90cb21
coderRohit620 <rohitravi620@gmail.com>
2026-03-11 09:58:13 +0530

    DB connection file setup

---
+++ chai-Backend/src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB = async () =>{
+    try {
+        const connetionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connetionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error",error);
+        process.exit(1)
+    }
+}
---
+++ chai-Backend/src/index.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+// 
+
+// function connectDB(){}
+// connectDB()
+
+
+/*
+
+import express from "express"
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`)
+        app.on("error", (error) =>{
+            console.log("ERROR",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () =>{
+            console.log(`App is Listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR",error)
+        throw err
+    }
+})()
+
+*/
