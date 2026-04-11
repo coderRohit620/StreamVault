50e093ebb4420c9e30937c963cee50c5253fc125
coderRohit620 <rohitravi620@gmail.com>
2026-04-12 00:09:20 +0530

    feat: add controllers and routes for comments, dashboard, healthcheck, likes, playlists, subscriptions, tweets, and videos

---
+++ chai-Backend/src/routes/comment.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import {
+    addComment ,
+    deleteComment,
+    getVideoComments,
+    updateComment,
+} from "../controllers/comment.controller.js"
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
